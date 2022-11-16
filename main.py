8d86d4db8d604e4aab9ac8044a930d050da640e8
AlexanderKlokov312 <klokovaa77@mail.ru>
2022-11-16 23:39:14 +0300

    First commit

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-# Yellow circles
+import random
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.flag = False
+        self.pushButton.clicked.connect(self.run)
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_yellow_circles(qp)
+            qp.end()
+
+    def run(self):
+        self.flag = True
+        self.repaint()
+
+    def draw_yellow_circles(self, qp):
+        x = random.randint(110, 450)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(100, 100, x, x)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
